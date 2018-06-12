81b3aca842f667c2d9165c558e53c863e4327525
Aon1840 <aonattapon1840.ap@gmail.com>
2018-06-12 23:20:29 +0700

    Add Crashlytics

--- app/src/main/java/com/example/aon_attapon/mvcstructure/MainApplication.java
+++ app/src/main/java/com/example/aon_attapon/mvcstructure/MainApplication.java
@@ -3,12 +3,15 @@ package com.example.aon_attapon.mvcstructure;
 import android.app.Application;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         //Initialize thing(s) here
 
         Contextor.getInstance().init(getApplicationContext());
