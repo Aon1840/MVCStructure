48d26036b8be5fed39a61c851057d81030d5e2c6
Aon1840 <aonattapon1840.ap@gmail.com>
2018-06-12 17:37:40 +0700

    PopBackStack

--- app/src/main/java/com/example/aon_attapon/mvcstructure/fragment/SecondFragment.java
+++ app/src/main/java/com/example/aon_attapon/mvcstructure/fragment/SecondFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.aon_attapon.mvcstructure.R;
 
@@ -13,6 +14,8 @@ import com.example.aon_attapon.mvcstructure.R;
  * Created by nuuneoi on 11/16/2014.
  */
 public class SecondFragment extends Fragment {
+
+    Button btnOk;
 
     public SecondFragment() {
         super();
@@ -35,6 +38,13 @@ public class SecondFragment extends Fragment {
 
     private void initInstances(View rootView) {
         // Init 'View' instance(s) with rootView.findViewById here
+        btnOk = (Button) rootView.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().popBackStack();
+            }
+        });
     }
 
     @Override
